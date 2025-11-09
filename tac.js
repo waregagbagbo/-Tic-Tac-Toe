1025c0a20932ea9e5b938ca22983016021c8f861
Default Name <gabelwarega@gmail.com>
2025-11-09 16:34:28 +0300

    setting the base object of the game

---
+++ tac.js
@@ -0,0 +1,32 @@
+// start by creating an empty board with arr of nine cell inside a GameBoard Object
+
+const GameBoard = (() => {
+
+    // array that represents the 3 by 3 board
+    let board = ["", "", "", "", "", "", "", "", ""];
+    
+    // implement function to see the board
+
+    const getBoard = () => board;
+
+    // function to place a mark on the board
+
+    const setCell = (index, mark) => {
+        if (index >= 0 && index < 9 && board[index] === "") {
+            board[index] = mark;
+           
+        }
+    };
+
+    // function to reset the board
+
+    const resetBoard = () => {
+        board = ["", "", "", "", "", "", "", "", ""];
+    };
+
+    return { getBoard, setCell, resetBoard }; // return the functions to interact with the boardclear
+
+    
+})();
+
+console.log(GameBoard())
